74488d3daeca0a3b595dfad3bb1cf63d206b4373
SomyaK18 <somya.kakarla@zemosolabs.com>
2024-06-21 12:56:28 +0530

    Update functionsassignment.js

--- functionsassignment.js
+++ functionsassignment.js
@@ -16,5 +16,3 @@ fName='Somya';
 lName='Kakarla';      
 let res=getInitials(fName,lName);
 console.log("First letter of "+fName+" "+lName+" is: "+res);
-let res1=getInitials('Roger','Waters');
-console.log("First letter of Roger Waters is: "+res1);
